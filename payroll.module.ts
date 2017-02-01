d83456f8a33fe2bd6d80535ff17d759ec9a7f025
Johan Alvarez <llstarscreamll@hotmail.com>
2017-01-31 23:42:09 -0500

    avance de asistente para configuración del módulo

--- payroll.module.ts
+++ payroll.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PayrollRoutingModule } from './payroll-routing.module';
-// Components
+// ng2-bootstrap
+import { TabsModule } from 'ng2-bootstrap/tabs';
+// AdminLTE Shell
+import { AdminLTEShellModule as Shell } from './../../shells/adminLTE/admin-lte.module';
+// Payroll Components
 import { SetupWizardComponent } from './components/setup-wizard/setup-wizard.component';
+// Company Components imports
+import { SharedCompanyModule } from './../company/sharedModule/shared-company.module';
 
 @NgModule({
   imports: [
     CommonModule,
-    PayrollRoutingModule
+    Shell,
+    PayrollRoutingModule,
+    SharedCompanyModule,
+    TabsModule.forRoot(),
   ],
-  declarations: [SetupWizardComponent]
+  declarations: [
+    SetupWizardComponent,
+  ]
 })
 export class PayrollModule { }
